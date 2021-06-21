44a5099880e091637dde8f90a7dea9ff7b51d9a0
subikshajaya <58353212+subikshajaya@users.noreply.github.com>
2021-06-21 22:53:07 +0530

    Login page UI

--- myreceipe/src/app/login-page/login-page.component.ts
+++ myreceipe/src/app/login-page/login-page.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
+  model: any = {};
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  togglePassword(){
+    var password=(<HTMLInputElement>document.getElementById("passWord"));
+    if(password.type=="password")
+    {
+      password.type="text";
+    }
+    else
+    {
+      password.type="password";
+    }
+  }
 }
